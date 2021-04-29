ffc159a352828b4f1365e6d867f7ae633eba60c6
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-29 16:58:15 -0500

    add footer component site-wide

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
+    <div>
       <div className="page-container">
         <Navbar />
         <Switch>
@@ -21,6 +23,8 @@ function App() {
         </Switch>
         <div className="push"></div>
       </div>
+      <Footer />
+    </div>
   );
 }
 
