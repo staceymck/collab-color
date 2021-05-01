afc3592698cda86284adf3d0810cde53af602381
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-01 14:46:34 -0500

    create CanvasList component

---
+++ src/components/CanvasList/CanvasList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CanvasList = (props) => {
+  return (
+    <div>
+      <h1>Select a canvas</h1>
+    {/* Maybe map over a list of the svgs passed in via props? */}
+    </div>
+  )
+}
+
+export default CanvasList
