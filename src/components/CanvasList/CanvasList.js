cc313e500ce31512447704ce66cf8b208403dd27
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-01 17:52:09 -0500

    map through canvases for display

--- src/components/CanvasList/CanvasList.js
+++ src/components/CanvasList/CanvasList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Canvas from '../Canvas/Canvas'
 
-const CanvasList = (props) => {
+const CanvasList = ({canvases}) => {
   return (
     <div>
       <h1>Select a canvas</h1>
-    {/* Maybe map over a list of the svgs passed in via props? */}
+      {canvases.map(c => <Canvas key={c.id} id={c.id} polygons={c.polygons} />)}
     </div>
   )
 }
