0a26b9dc10e3f4399705c02f4e8ad2a2565f47b2
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-03 16:19:09 -0500

    align action types with action creator names

--- src/actions/canvasActions.js
+++ src/actions/canvasActions.js
@@ -1,10 +1,9 @@
-
-export const fetchCanvasesSuccess = canvases => ({type: "CANVASES_FETCH_SUCCESS", payload: canvases})
-export const fetchCanvasesError = error => ({type: "CANVASES_FETCH_ERROR", payload: error})
+export const fetchCanvasesSuccess = canvases => ({type: "FETCH_CANVASES_SUCCESS", payload: canvases})
+export const fetchCanvasesError = error => ({type: "FETCH_CANVASES_ERROR", payload: error})
 
 export const fetchCanvasesStart = () => {
   return (dispatch) => {
-    dispatch({type: "CANVASES_FETCH_START"})
+    dispatch({type: "FETCH_CANVASES_START"})
 
     fetch("http://localhost:3000/canvases")
     .then(res => res.json())
