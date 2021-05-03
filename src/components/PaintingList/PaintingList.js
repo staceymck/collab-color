33b2a98d565ff1dbec86cbf6bf4f057b1f566446
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-03 16:27:42 -0500

    set up display grid for gallery

---
+++ src/components/PaintingList/PaintingList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Canvas from '../Canvas/Canvas'
+import styles from './PaintingList.module.css';
+
+const PaintingList = ({paintings}) => {
+  return (
+    <div className={styles.grid}>
+      {paintings.map(p => <Canvas key={p.id} id={p.id} polygons={p.polygons} cardStyle={styles.card} />)}
+    </div>
+  )
+}
+
+export default PaintingList
