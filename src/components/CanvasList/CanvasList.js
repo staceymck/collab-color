39bcc190863106a9a508f78d674a8eea911d99d7
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-03 16:21:13 -0500

    add styles

--- src/components/CanvasList/CanvasList.js
+++ src/components/CanvasList/CanvasList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Canvas from '../Canvas/Canvas'
+import styles from './CanvasList.module.css'
 
 const CanvasList = ({canvases}) => {
+  console.log("canvaslist")
   return (
     <div>
       <h1>Select a canvas</h1>
-      {canvases.map(c => <Canvas key={c.id} id={c.id} polygons={c.polygons} />)}
+      <div className={styles.row}>
+        {canvases.map(c => <Canvas key={c.id} id={c.id} polygons={c.polygons} cardStyles={styles.card} />)}
+      </div>
     </div>
   )
 }
