78f5fafcf3e85bbbf1c2819021b5a9025b4d85d7
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-04 10:04:22 -0500

    add flexibility to stroke width

--- src/components/Polygon/Polygon.js
+++ src/components/Polygon/Polygon.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Polygon = ({points, id, fill}) => {
+const Polygon = ({points, id, fill, strokeWidth}) => {
   return (
-    <polygon id={id} key={id} points={points} fill={fill} stroke="#231f20" strokeLinejoin="round" strokeWidth="0.25px" />
+    <polygon id={id} key={id} points={points} fill={fill} stroke="#231f20" strokeLinejoin="round" strokeWidth={strokeWidth} />
   )
 }
 
