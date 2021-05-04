f129a0077a95ee4dc20452c0769d414524037cf0
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-03 22:08:41 -0500

    add loading animation

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export default class Loading extends React.Component {
+
+  state = {
+    dark: 1,
+    dots: 3
+  }
+
+  componentDidMount = () => {
+    this.interval = setInterval(() => {
+      if (this.state.dark === this.state.dots) {
+        this.setState({dark: 1})
+      } else if (this.state.dark >= 1 && this.state.dark < this.state.dots) {
+        this.setState(state => {
+          return (
+            {dark: state.dark + 1}
+          )
+        })
+      }
+    }, 200) 
+  }
+
+  componentDidUnMount = () => {
+    clearInterval(this.interval)
+  }
+
+  displayDots = () => {
+    const dots = [...Array(this.state.dots).keys()].map(x => x += 1)
+    return dots.map(dot => {
+      return (
+        <svg height="100" width="100">
+          <circle cx="50" cy="50" r="20" fill={this.state.dark === dot ? "#267E92" : "#43BEC6" } />
+        </svg>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Loading</h1>
+        {this.displayDots()}
+      </div> 
+    )
+  }
+}
+
