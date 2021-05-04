60e29edfa0eab1ba1e5e179b110cf916c17cb71d
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-04 18:51:01 -0500

    wrap canvases in links

--- src/components/CanvasList/CanvasList.js
+++ src/components/CanvasList/CanvasList.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import Canvas from '../Canvas/Canvas'
-import styles from './CanvasList.module.css'
+import Canvas from '../Canvas/Canvas';
+import styles from './CanvasList.module.css';
+import { Link } from 'react-router-dom';
 
 const CanvasList = ({canvases}) => {
-  console.log("canvaslist")
   return (
     <div>
-      <h1>Select a canvas</h1>
+      <h1 className={styles.header}>Select a canvas</h1>
       <div className={styles.row}>
-        {canvases.map(c => <Canvas key={c.id} id={c.id} polygons={c.polygons} cardStyles={styles.card} />)}
+        {canvases.map(c => {
+          return (
+            <Link to={`/canvases/${c.id}/paintings/new`} className={styles.canvas}> 
+              <Canvas 
+                key={c.id} 
+                id={c.id}
+                polygons={c.polygons}
+                cardStyles={styles.card} 
+                strokeWidth="0.5px"
+              />
+            </Link>
+          )}
+        )}
       </div>
     </div>
   )
