8981e69dd9b36d1e77834aae9b17ed8847c5d2fe
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-04 10:05:45 -0500

    build out initial canvas container with routes

---
+++ src/containers/CanvasesContainer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import CanvasList from '../components/CanvasList/CanvasList';
+import { fetchCanvasesStart } from '../actions/canvasActions';
+import Canvas from "../components/Canvas/Canvas";
+
+class CanvasesContainer extends React.Component {
+
+  componentDidMount = () => {
+    this.props.fetchCanvasesStart();
+  }
+
+  selectStudioCanvas = (routerProps) => {
+    const id = parseInt(routerProps.match.params.id, 10)
+    const canvas = this.props.canvases.find(c => c.id === id)
+    if (canvas) {
+      return (
+        <Canvas 
+          key={canvas.id}
+          id={canvas.id}
+          polygons={canvas.polygons}
+          //cardStyles={styles.studioCanvas}  //add studio style
+        />
+      )
+    } else {
+      return <div>Canvas not found!</div> //handle errors - maybe redirect to canvas list route
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+       <Switch> 
+         <Route exact path="/canvases">
+           <CanvasList canvases={this.props.canvases}/>
+        </Route>
+        <Route 
+          path="/canvases/:id/paintings/new"
+          render={(routeProps) => this.selectStudioCanvas(routeProps) } /> 
+      </Switch> 
+    </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    status: state.canvases.status,
+    canvases: state.canvases.canvases
+  }
+}
+
+export default connect(mapStateToProps, {fetchCanvasesStart})(CanvasesContainer)
