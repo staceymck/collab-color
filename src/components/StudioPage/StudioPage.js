5f6d707f769437743ac0937de5cb44ecc0120da1
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-05 10:32:48 -0500

    build out painting feature

--- src/components/StudioPage/StudioPage.js
+++ src/components/StudioPage/StudioPage.js
@@ -1,20 +1,63 @@
 import React from 'react';
-import Canvas from '../Canvas/Canvas';
+import StudioCanvas from '../StudioCanvas/StudioCanvas';
 import styles from './StudioPage.module.css';
 import { SketchPicker } from 'react-color';
+import buttonStyles from '../Button/Button.module.css';
+import { addColor, resetCanvas, createPaintingStart } from '../../actions/studioActions';
+import { connect } from 'react-redux';
+import ColorPicker from '../ColorPicker/ColorPicker'
 
-const StudioPage = ({canvas}) => {
-  return (
-    <div>
-      <Canvas 
-        key={canvas.id}
-        id={canvas.id}
-        polygons={canvas.polygons}
-        // cardStyles={add style}
-      />
-      <SketchPicker />
-    </div>
-  )
+class StudioPage extends React.Component {
+
+  state = {
+    activeColor: "#267E92"
+  }
+
+  componentDidMount = () => {
+    this.props.newPainting(this.props.canvas.polygons)
+  }
+
+  handleChangeComplete = (color) => {
+    this.setState({activeColor: color.hex})
+  }
+
+  render() {
+    const { studioCanvas, addColor, resetCanvas, createPaintingStart, canvas } = this.props
+
+    return ( 
+      <div className={styles.container}>
+        <StudioCanvas 
+          polygons={studioCanvas}
+          cardStyles={styles.item1}
+          onClick={addColor}
+          activeColor={this.state.activeColor}
+        />
+
+        <div className={styles.item2}>
+          <SketchPicker
+            color={this.state.activeColor}
+            onChangeComplete={this.handleChangeComplete}
+            className={styles.colorPicker}
+          />
+        </div>
+
+        <div className={styles.item3}>
+          <button className={buttonStyles.primary} onClick={() => createPaintingStart(canvas.id, studioCanvas)}>
+            Save to gallery
+          </button>
+          <button onClick={resetCanvas}>
+            Reset
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default StudioPage
+const mapStateToProps = (state) => {
+  return {
+    studioCanvas: state.studioCanvas.studioCanvas
+  }
+}
+
+export default connect(mapStateToProps, {addColor, resetCanvas, createPaintingStart})(StudioPage)
