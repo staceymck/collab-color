6f3d4fd64bd88a971957ad8c8cba4c2b3022e047
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-05 15:53:40 -0500

    track query value in state

--- src/actions/paintingActions.js
+++ src/actions/paintingActions.js
@@ -1,13 +1,16 @@
 export const fetchPaintingsSuccess = paintings => ({type: "FETCH_PAINTINGS_SUCCESS", payload: paintings})
 export const fetchPaintingsError = error => ({type: "FETCH_PAINTINGS_ERROR", payload: error})
 
-export const fetchPaintingsStart = () => {
+export const fetchPaintingsStart = (query = "newest", page = 1) => {
   return (dispatch) => {
+    dispatch(setSortQuery(query))
     dispatch({type: "FETCH_PAINTINGS_START"})
 
-    fetch("http://localhost:3000/paintings")
+    fetch(`http://localhost:3000/paintings?q=${query}&page=${page}`)
     .then(res => res.json())
     .then(data => {
+      console.log(data.paintings)
+      dispatch(setPagination(data.current, data.total))
       dispatch(fetchPaintingsSuccess(data.paintings))
     })
     .catch(error => {
@@ -19,3 +22,7 @@ export const fetchPaintingsStart = () => {
 export const setPagination = (current, total) => {
   return {type: "SET_PAGINATION", currentPage: current, totalPages: total}
 } 
+
+export const setSortQuery = (query) => {
+  return {type: "SET_SORT_QUERY", query: query}
+}
