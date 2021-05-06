9d1ee73eff469bb86aca8878b65a954702e1af7d
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-06 13:37:07 -0500

    make pagination buttons links since changing url

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Pagination = ({current, total, fetch, currentQuery}) => {
+
+  const prevPath = () => {
+    if (currentQuery) {
+      return `/gallery?q=${currentQuery}&page=${parseInt(current - 1)}`
+    } else {
+      return `/gallery?page=${parseInt(current - 1)}`
+    }
+  }
+
+  const nextPath = () => {
+    if (currentQuery) {
+      return `/gallery?q=${currentQuery}&page=${parseInt(current + 1)}`
+    } else {
+      return `/gallery?page=${parseInt(current + 1)}`
+    }
+  }
+
   return (
     <div>
-      {current !== 1 && <p onClick={() => fetch(currentQuery, parseInt(current - 1))}>Previous</p> }
-      {current === total && <p onClick={() => fetch(currentQuery, parseInt(current + 1))}>Next</p> }
+      {current !== 1 && <Link to={prevPath()} onClick={() => fetch(currentQuery, parseInt(current - 1))}>Previous</Link> }
+      {current !== total && <Link to={nextPath()} onClick={() => fetch(currentQuery, parseInt(current + 1))}>Next</Link> }
     </div>
   )
 }
