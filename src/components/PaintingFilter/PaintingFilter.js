5718f9f589e140b241c58bbaa246439eb1476192
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-06 15:50:00 -0500

    add styles to indicate active filter

--- src/components/PaintingFilter/PaintingFilter.js
+++ src/components/PaintingFilter/PaintingFilter.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import styles from './PaintingFilter.module.css'
 
-
-const PaintingFilter = ({fetch}) => {
+const PaintingFilter = ({fetch, currentQuery}) => {
   
   const filters = ["Newest", "Oldest", "Random"]
 
   return (
-    <div>
-      Change the pattern: 
+    <div className={styles.container}>
+      <span className={styles.cta}>Change the pattern: </span>
       {filters.map(filter => {
         const filterLowercase = filter.toLowerCase()
         return (
           <Link
+            className={currentQuery === filterLowercase ? styles.activeFilter : styles.filter}
             to={`/gallery?q=${filterLowercase}&page=1`}
             onClick={() => fetch(filterLowercase)}>{filter}
+            
           </Link>
         )
       })}
