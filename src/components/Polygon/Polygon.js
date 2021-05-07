4a11e9b564b63d967e474f3cfbb3b8c550dff12d
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-07 08:48:00 -0500

    add onClick callback option

--- src/components/Polygon/Polygon.js
+++ src/components/Polygon/Polygon.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const Polygon = ({points, id, fill, strokeWidth}) => {
+const Polygon = ({points, id, fill, strokeWidth, onClick}) => {
   return (
-    <polygon id={id} key={id} points={points} fill={fill} stroke="#231f20" strokeLinejoin="round" strokeWidth={strokeWidth} />
+    <polygon
+      id={id}
+      key={id}
+      points={points}
+      fill={fill} stroke="#231f20"
+      strokeLinejoin="round"
+      strokeWidth={strokeWidth}
+      onClick={onClick || null}
+    />
   )
 }
 
