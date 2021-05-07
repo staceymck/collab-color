5db019331f8cdf6a8999663d4fceb9b4396a08e4
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-07 15:55:33 -0500

    use handle errors with errorReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import canvasReducer from './canvasReducer';
 import paintingReducer from './paintingReducer';
 import studioReducer from './studioReducer';
+import errorReducer from './errorReducer';
 
 const rootReducer = combineReducers({
   canvases: canvasReducer,
   paintings: paintingReducer,
-  studioCanvas: studioReducer
+  studioCanvas: studioReducer,
+  errors: errorReducer
 })
 
 export default rootReducer
