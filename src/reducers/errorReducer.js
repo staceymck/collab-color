511705171e37de76e86a3abf8aea6ebd3737d34d
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-07 16:44:15 -0500

    handle errors with separate reducer

---
+++ src/reducers/errorReducer.js
@@ -0,0 +1,23 @@
+const errorReducer = (
+  state = {error: null, isOpen: false},
+  action
+) => {
+  const { error } = action;
+ 
+  if(error) {
+    console.log(error + " show")
+    return {
+      error: error.message, //how do the errors come back? could I have more than one?
+      isOpen: true
+    }
+  } else if(action.type === "HIDE_ERROR") {
+    console.log("hide")
+    return {
+      error: null,
+      isOpen: false
+    }
+  }
+  return state;
+ }
+
+ export default errorReducer
