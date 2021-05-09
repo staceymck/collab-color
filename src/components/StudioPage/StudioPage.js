456e0ae9ba3a63afbdcb0f4c66b777d5b143ad50
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-08 20:46:17 -0500

    use recently used colors as presets in color picker

--- src/components/StudioPage/StudioPage.js
+++ src/components/StudioPage/StudioPage.js
@@ -5,15 +5,16 @@ import { SketchPicker } from 'react-color';
 import buttonStyles from '../Button/Button.module.css';
 import { addColor, resetCanvas, createPaintingStart, resetStatus } from '../../actions/studioActions';
 import { connect } from 'react-redux';
-import ColorPicker from '../ColorPicker/ColorPicker'
-
 import Loading from '../Loading/Loading';
 import SuccessModal from '../SuccessModal/SuccessModal';
 
 class StudioPage extends React.Component {
 
   state = {
-    activeColor: "#267E92"
+    activeColor: "#267E92",
+    recentColors: ['#D0021B', '#F5A623', '#F8E71C', '#8B572A', '#7ED321', '#417505',
+    '#BD10E0', '#9013FE', '#4A90E2', '#50E3C2', '#B8E986', '#000000',
+    '#4A4A4A', '#9B9B9B', '#FFFFFF']
   }
 
   componentDidMount = () => {
@@ -28,6 +29,23 @@ class StudioPage extends React.Component {
     this.setState({activeColor: color.hex})
   }
 
+  handleClick = (id, activeColor) => {
+    this.props.addColor(id, activeColor)
+
+    if (!this.state.recentColors.includes(activeColor.toUpperCase())) {
+      console.log(this.state.recentColors)
+      this.setState(state => {
+        const colors = state.recentColors.slice(0)
+        colors.unshift(activeColor.toUpperCase())
+        colors.pop()
+        return {
+          ...state,
+          recentColors: colors
+        }
+      }, () => this.state.recentColors)
+    }
+  } 
+
   minPolysPainted = () => {
     const painted = this.props.studioCanvas.filter(p => p.color !== "#fff" && p.color !== "#ffffff")
     return painted.length >= 20 ? true : false
@@ -38,21 +56,24 @@ class StudioPage extends React.Component {
     return ( 
       <div className={styles.container}>
         {status === "pending" && <Loading message="Posting" />}
-        {console.log(status)}
         {status === "resolved" && <SuccessModal />}
-        {/* <ColorPicker /> */}
+        
         <Canvas 
           polygons={studioCanvas}
           cardStyles={styles.item1}
-          onClick={addColor}
+          // onClick={addColor}
+          onClick={this.handleClick}
           activeColor={this.state.activeColor}
         />
-
+        
         <div className={styles.item2}>
           <SketchPicker
             color={this.state.activeColor}
             onChangeComplete={this.handleChangeComplete}
             className={styles.colorPicker}
+            width="60%"
+            disableAlpha={true}
+            presetColors={this.state.recentColors}
           />
         </div>
 
