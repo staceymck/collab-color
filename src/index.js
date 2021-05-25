8b7acafcb7e1017d6201a6c54d343cc6b9711228
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-25 14:05:54 -0500

    handle scroll restoration for react router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
+import './index.css';
+import App from './App';
+import ScrollToTop from './ScrollToTop';
+
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
@@ -16,6 +18,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
+        <ScrollToTop />
         <App />
       </Router>
     </Provider>
